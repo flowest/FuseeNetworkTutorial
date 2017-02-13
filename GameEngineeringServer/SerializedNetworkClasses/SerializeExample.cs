07a19e09b76706f49f829da1db9b8ff02b2768fc
Flo <flooow18@gmail.com>
2017-02-13 12:38:09 +0100

    added serialized class

---
+++ GameEngineeringServer/SerializedNetworkClasses/SerializeExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProtoBuf;
+
+namespace SerializedNetworkClasses
+{
+    [ProtoContract]
+    public class SerializeExample
+    {
+        [ProtoMember(1)]
+        public string exampleString;
+
+        [ProtoMember(2)]
+        public float exampleFloat;
+    }
+}
---
+++ GameEngineeringServer/SerializedNetworkClasses/ext/executeProtoBufPrecompile.bat
@@ -0,0 +1,1 @@
+%FuseeRoot%\ext\protobuf\PrecompileTool\precompile %~dp0\..\bin\Debug\SerializedNetworkClasses.dll -o:%~dp0\..\bin\Debug\NetworkClassesSerializer.dll -t:NetworkClassesSerializer
